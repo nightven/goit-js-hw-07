fcfac6bc6b5c79b90e915ef281a8c7a858cca34b
Vitaliy Beyar <37002507+nightven@users.noreply.github.com>
2023-07-10 18:42:20 +0200

    add started files

---
+++ js/01-gallery.js
@@ -0,0 +1,19 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const listGalleryEl = document.querySelector(".gallery");
+console.log(createGalleryMarkup(galleryItems));
+
+setGalleryToListHTML(`beforeend`, createGalleryMarkup(galleryItems));
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ description, preview }) => `
+  <li><a href=""><img class ="gallery__image" src="${preview}" alt="${description}" ></a></li>`
+    )
+    .join("");
+}
+
+function setGalleryToListHTML(place, gallery) {
+  listGalleryEl.insertAdjacentHTML(place, gallery);
+}
