0e92b4c21b764cb7c15dbe1c170a5cf32a5165c7
Vitaliy Beyar <37002507+nightven@users.noreply.github.com>
2023-07-11 08:38:13 +0200

    added 2 gallery library

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,44 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
 const listGalleryEl = document.querySelector(".gallery");
-console.log(createGalleryMarkup(galleryItems));
+listGalleryEl.addEventListener("click", onClickAtPicture);
 
 setGalleryToListHTML(`beforeend`, createGalleryMarkup(galleryItems));
+
+function onClickAtPicture(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  showBigImg(e.target);
+}
+function showBigImg(target) {
+  const instance = basicLightbox.create(
+    `<img src="${target.dataset.source}" width="800" height="600">`
+  );
+  instance.show();
+
+  listGalleryEl.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+
 function createGalleryMarkup(galleryItems) {
   return galleryItems
     .map(
-      ({ description, preview }) =>
-        `<li><a href=""><img class ="gallery__image" src="${preview}" alt="${description}"></a></li>`
+      ({ description, preview, original }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
     )
     .join("");
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector(".gallery");
+galleryListEl.addEventListener("click", onClickToImg);
+
+function onClickToImg(e) {
+  e.preventDefault();
+
+  let lightbox = new SimpleLightbox(".gallery a", {
+    // captions: true,
+    captionData: "alt",
+    // captionPosition: "bottom",
+    captionDelay: 250,
+  });
+}
+
+gallerySetToHtml(createGalleryMarkup2(galleryItems));
+
+function createGalleryMarkup2(galleryItems) {
+  return galleryItems
+    .map(
+      ({ description, preview, original }) =>
+        `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+             <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>
+       </li>`
+    )
+    .join("");
+}
+
+function gallerySetToHtml(gallery) {
+  galleryListEl.innerHTML = gallery;
+}
