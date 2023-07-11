f3738a32badb122c82470d6a14bc511ed24c7229
Vitaliy Beyar <37002507+nightven@users.noreply.github.com>
2023-07-11 08:57:09 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,20 +1,15 @@
 import { galleryItems } from "./gallery-items.js";
 
 const galleryListEl = document.querySelector(".gallery");
-galleryListEl.addEventListener("click", onClickToImg);
-
-function onClickToImg(e) {
-  e.preventDefault();
-
-  let lightbox = new SimpleLightbox(".gallery a", {
-    // captions: true,
-    captionData: "alt",
-    // captionPosition: "bottom",
-    captionDelay: 250,
-  });
-}
 
 gallerySetToHtml(createGalleryMarkup2(galleryItems));
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
 
 function createGalleryMarkup2(galleryItems) {
   return galleryItems
